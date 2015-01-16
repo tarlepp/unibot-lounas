eb73724ccb192291e4cd5844e1b81e400395e465
tarlepp <tarmo.leppanen@kase.fi>
2015-01-16 23:04:23 +0200

    Added Best and Trattoria. Also fixed Vanha Asemaravintola data fetch. + Added simple test script.

--- index.js
+++ index.js
@@ -66,10 +66,104 @@ module.exports = function init(options) {
                 if (data == null) {
                     callback(null, null);
                 } else {
+                    var days = [
+                        'Sunnuntai',
+                        'Maanantai',
+                        'Tiistai',
+                        'Keskiviikko',
+                        'Torstai',
+                        'Perjantai',
+                        'Lauantai'
+                    ];
+
+                    var date = new Date();
+                    var day = date.getDate();
+                    var month = date.getMonth() + 1;
+
+                    if (day < 10) {
+                        day = '0' + day;
+                    }
+
+                    if (month < 10) {
+                        month = '0' + month;
+                    }
+
+                    var dayString = days[date.getDay()] + ' ' + day + '.' + month;
                     var $ = cheerio.load(data);
-                    var dishes = $('div.rightcolumn p').eq(1).text().split('\n').slice(1) || [];
+                    var dishes = $('div.content').find('h2:contains("' + dayString + '")').next().text().split('\n');
 
                     callback(null, dishes.join(', '));
+                }
+            });
+        }
+
+        /**
+         * Helper function to fetch lunch data for Trattoria Aukio from Raflaamo website.
+         *
+         * @param {Function}    callback    Callback function
+         */
+        function fetchAukio(callback) {
+            helpers.download('https://www.raflaamo.fi/fi/jyvaskyla/trattoria-aukio', function success(data) {
+                if (data == null) {
+                    callback(null, null);
+                } else {
+                    var $ = cheerio.load(data);
+                    var dishes = [];
+                    var currentDayElement = $('#days-chooser').find('div.day-option.active');
+                    var currentDate = currentDayElement.data('date');
+                    var currentDay = currentDayElement.find('span.long').text().trim();
+
+                    $('#lunches-holder')
+                        .find('span.long:contains("' + currentDay + ' ' + currentDate +'")')
+                        .closest('.lunch-day').find('h4')
+                        .each(function iterator() {
+                            dishes.push($(this).find('span').eq(0).text().trim());
+                        });
+
+                    // Remove "week" dish
+                    if (dishes.length > 1) {
+                        dishes.pop();
+                    }
+
+                    callback(null, dishes.join(', '));
+                }
+            });
+        }
+
+        /**
+         * Helper function to fetch lunch data for Pizzeria Best
+         *
+         * @param {Function}    callback    Callback function
+         */
+        function fetchBest(callback) {
+            helpers.download('http://www.pizzeriabest.fi/onlinetilaus/index.php?main_page=page&id=1', function success(data) {
+                if (data == null) {
+                    callback(null, null);
+                } else {
+                    var $ = cheerio.load(data);
+                    var days = [
+                        'Su',
+                        'Ma',
+                        'Ti',
+                        'Ke',
+                        'To',
+                        'Pe',
+                        'La'
+                    ];
+
+                    var lines = _.map($('div.centerColumn div').text().match(/[^\r\n]+/g), function iterator(line) {
+                        return line.trim();
+                    });
+
+                    var date = new Date();
+                    var match = new RegExp('^' + days[date.getDay()] + ': ');
+                    var dishes = _.find(lines, function iterator(line) {
+                        if (match.test(line)) {
+                            return true;
+                        }
+                    });
+
+                    callback(null, dishes);
                 }
             });
         }
@@ -80,7 +174,9 @@ module.exports = function init(options) {
                 // Define jobs
                 var jobs = {
                     'Shalimar': fetchShalimar,
-                    'Asemaravintola': fetchAsema
+                    'Asemaravintola': fetchAsema,
+                    'Trattoria': fetchAukio,
+                    'Best': fetchBest
                 };
 
                 // Fetch lunch data parallel
